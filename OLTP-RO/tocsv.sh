e46d962fa50cf7c487a438ccc9f653c036623f43
VadimTk <apachephp@gmail.com>
2016-03-22 01:12:57 +0000

    Summaries added

---
+++ OLTP-RO/tocsv.sh
@@ -0,0 +1,9 @@
+rm -f summary.csv
+for t in  mysql57
+do
+for i in 1 2 3 4 5 6 8 10 13 16 20 25 31 38 46 56 68 82 100 120 145 175 210 250 300 360 430 520 630 750 870 1000
+do
+echo "res.$t/res.${i}.ro.txt $t,$i"
+bash parse.sh raw/res.oltpRO.$t/res.${i}.ro.txt $t,$i >> summary.csv
+done
+done
