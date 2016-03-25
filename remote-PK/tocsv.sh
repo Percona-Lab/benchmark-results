1de1e14b687e5ba699e294b4e231a0b19c4ab701
root <root@smblade01.dur.percona.com>
2016-03-25 19:52:39 -0400

    Added mysql56.itc64

--- remote-PK/tocsv.sh
+++ remote-PK/tocsv.sh
@@ -1,7 +1,7 @@
 rm -f summary.csv
 for i in 1 2 3 4 5 6 8 10 13 16 20 25 31 38 46 56 68 82 100 120 145 175 210 250 300 360 430 520 630 750 870 1000
 do
-for t in mysql55.tiRC.full mysql56.tiRC.full mysql57.tiRC.full
+for t in mysql55.tiRC.full mysql56.tiRC.full mysql57.tiRC.full mysql56.itc64
 do
 bash parse.sh  raw/res.${t}/res.thr${i}.ro.txt $t,$i >> summary.csv
 done
