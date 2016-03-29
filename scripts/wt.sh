bd93856e0be46c11b3ffe1ac889ab8ed8546ceca
Fernando Ipar <fipar@acm.org>
2016-03-28 23:34:24 -0300

    set cache size for wt to 64GB

--- scripts/wt.sh
+++ scripts/wt.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-cat <<EOF>wt.config
-# setParameter:
-#     storage.wiredTiger.collectionConfig.configString:"type=lsm"
-#     storage.wiredTiger.indexConfig.configString:"type=lsm"
-EOF
-
 numactl --interleave=all ./percona-server-mongodb-3.0.8-1.2/bin/mongod \
   $* \
   --dbpath=/mnt/i3600/PERF-15/wt \
   --storageEngine=wiredTiger \
-  --config ./wt.config #\
+  --wiredTigerCacheSizeGB=64
+#  --config ./wt.config #\
 #  --auth
 
