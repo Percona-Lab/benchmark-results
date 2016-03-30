b07c8a7d2e00df7b68b35da52874d003e50011df
VadimTK <vadim@percona.com>
2016-03-30 14:46:48 -0400

    local-OLTP added

---
+++ local-OLTP/tocsv.sh
@@ -0,0 +1,10 @@
+rm -f summary.csv
+for i in 1 2 3 4 5 6 8 10 13 16 20 25 31 38 46 56 68 82 100 120 145 175 210 250 300 360 430 520 630 750 870 1000
+do
+for t in raw/res*
+do
+res=$(basename "$t")
+res=${res#res.} 
+bash parse.sh  ${t}/res.thr${i}.ro.txt $res,$i >> summary.csv
+done
+done
