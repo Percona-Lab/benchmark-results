26c54ae9248157446bae9b5c8e15b324c33c3f88
Fernando Ipar <fipar@acm.org>
2016-04-07 10:47:42 -0300

    added results for PSfM 3.2

--- scripts/to_csv.sh
+++ scripts/to_csv.sh
@@ -3,17 +3,17 @@
 #It must be executed from within the scripts directory, as it uses relative paths
 
 for profile in 0 2; do
-    env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/sysbench-wt-2000000-1-oltp-profile$profile.txt pl$profile 2000000 1 > ../alldata-pl$profile.csv
+    env _ONLYHEADER=1 csv_from_sysbench.sh ../raw${VERSION}/sysbench-${FULLVERSION}wt-2000000-1-oltp-profile$profile.txt pl$profile 2000000 1 > ../alldata-pl$profile$VERSION.csv
     for workload in oltp oltp_ro; do
-	for f in ../raw/sysbench-wt-*$workload-profile$profile.txt; do 
+	for f in ../raw${VERSION}/sysbench-${FULLVERSION}wt-*$workload-profile$profile.txt; do 
 	    threads=$(echo $f|sed 's/.*2000000-//g'|sed 's/-oltp.*//g')
-	    env _NOHEADER=1 csv_from_sysbench.sh $f $workload 2000000 $threads >> ../alldata-pl$profile.csv
+	    env _NOHEADER=1 csv_from_sysbench.sh $f $workload 2000000 $threads >> ../alldata-pl$profile$VERSION.csv
 	done
     done
 done
-echo "profile_level,$(head -1 ../alldata-pl0.csv)" > ../alldata.csv
+echo "profile_level,$(head -1 ../alldata-pl0$VERSION.csv)" > ../alldata$VERSION.csv
 for profile in 0 2; do
-   cat ../alldata-pl$profile.csv | grep -v workload|while read l; do
-				      echo "pl$profile,$l" >> ../alldata.csv
+   cat ../alldata-pl$profile$VERSION.csv | grep -v workload|while read l; do
+				      echo "pl$profile,$l" >> ../alldata$VERSION.csv
    done
 done
