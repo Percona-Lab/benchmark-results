7f09697a3003f75478fd88fd029f2bf6754c6218
Fernando Ipar <fipar@acm.org>
2016-04-09 00:55:06 -0300

    added profiling level 1 and disabling of transparent huge pages

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -9,13 +9,13 @@ kill -SIGTERM $(pidof mongod)
     done
     echo " Done"
 rm -rf /mnt/i3600/PERF-15/$engine/
-'cp' -rv /mnt/storage/PERF-15/$engine /mnt/i3600/PERF-15/
+'cp' -rv /mnt/storage/PERF-15/3.2-$engine /mnt/i3600/PERF-15/
+mv /mnt/i3600/PERF-15/3.2-$engine /mnt/i3600/PERF-15/$engine 
 pushd /mnt/storage/PERF-15/fipar/
-#./$engine.sh --quiet &> $engine.log &
-[ "$engine" == "ft" ] && ./transparent_huge_pages.sh disable || ./transparent_huge_pages.sh enable
-./$engine.sh --quiet $* &> $engine.log &
+./transparent_huge_pages.sh disable
+env VERSION=$version ./upstream-${engine}.sh --quiet $* &> upstream-${engine}.log &
    echo -n "Waiting for mongod ($engine) to become ready ..."
-    while [ $(echo 'db.isMaster()' | /mnt/storage/PERF-15/fipar/percona-server-mongodb-3.0.10-1.5/bin/mongo 2>/dev/null|grep ismaster|grep -c true) -eq 0 ]; do
+    while [ $(echo 'db.isMaster()' | /mnt/storage/PERF-15/fipar/mongodb-linux-x86_64-ubuntu1404-3.2.4/bin/mongo 2>/dev/null|grep ismaster|grep -c true) -eq 0 ]; do
        sleep 0.3
     done
     echo " Done"
@@ -25,7 +25,7 @@ popd
     size=2000000
     for threads in 1 4 8 16 32 64 256; do
        for workload in oltp oltp_ro; do
-	for extra in "--profile=0 --slowms=100000" "--profile=2 --slowms=0"; do
+	for extra in "--profile=0 --slowms=100000" "--profile=2 --slowms=0" "--profile=1 --slowms=2"; do
         profile=$(echo $extra|awk '{print $1}'|awk -F'=' '{print $2}')
 	echo "Workload: $workload, threads: $threads, engine: $engine"
 	echo -n "restoring datadir ..."
@@ -50,7 +50,7 @@ popd
 		--oltp-distinct-ranges=1 \
 		--oltp-index-updates=1 \
 		--oltp-non-index-updates=1 \
-		--oltp-inserts=1 run 2>&1 | tee sysbench-$engine-$size-$threads-$workload-profile$profile.txt
+		--oltp-inserts=1 run 2>&1 | tee sysbench-upstream-$engine-$size-$threads-$workload-profile$profile.txt
 	 echo "Benchmark complete"
 	done
        done
