02e79918f437f91f69fbefd4824c2d291162aea2
Fernando Ipar <fipar@acm.org>
2016-04-12 14:21:52 -0300

    updated wired tiger start script with the one used in the benchmark

--- scripts/wt.sh
+++ scripts/wt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 [ -z "$VERSION" ] && VERSION=3.0.8-1.2
 
 numactl --interleave=all ./percona-server-mongodb-$VERSION/bin/mongod \
   $* \
   --dbpath=/mnt/i3600/PERF-15/wt \
   --storageEngine=wiredTiger \
-  --config ./wt.config #\
-#  --auth
+  --wiredTigerCacheSizeGB=64
+
