ccbff2d66b07ab7566c355b085b4c4fe8e6739ce
Fernando Ipar <fipar@acm.org>
2016-05-28 23:50:43 -0300

    added memory scalability results. fixed to_csv.sh

--- scripts/to_csv.sh
+++ scripts/to_csv.sh
@@ -3,23 +3,30 @@
 #It must be executed from within the scripts directory, as it uses relative paths
 # sample file name for this benchmark: 
 # raw/thread_scalability/sysbench-rocks-ext4-rocks0-pareto-1-oltp.txt
-echo "engine,configuration,distribution,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/thread_scalability/sysbench-rocks-ext4-rocks0-pareto-1-oltp.txt _ _ _)" > ../thread_scalability.csv
+echo "engine,configuration,distribution,workload,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/thread_scalability/sysbench-rocks-ext4-rocks0-pareto-1-oltp.txt _ _ _)" > ../thread_scalability.csv
 
-THREADS="1 3 5 8 13 20 31 46 68 100 145 210 300 430 630 870 1000 1200"
+for f in ../raw/thread_scalability/sysbench*txt; do
+    engine=$(echo $f|awk -F'-' '{print $2}')
+    configuration=$(echo $f|awk -F'-' '{print $4}')
+    distribution=$(echo $f|awk -F'-' '{print $5}')
+    threads=$(echo $f|awk -F'-' '{print $6}')
+    workload=$(echo $f|awk -F'-' '{print $7}'|sed 's/\.txt//')
+    env _NOHEADER=1 csv_from_sysbench.sh $f psfm 60000000 $threads | while read l; do
+									 echo "$engine,$configuration,$distribution,$workload,$l" >> ../thread_scalability.csv
+								     done
+done # for f in ..
 
-for workload in oltp oltp_ro; do
-    for distribution in uniform pareto; do
-	for engine in wt rocks; do
-	    for threads in $THREADS; do
-		for configuration in rocks0 wt0 wt1 wt2; do
-		    echo $configuration|grep $engine>/dev/null || continue
-		    for f in ../raw/thread_scalability/sysbench-$engine-ext4-$configuration-$distribution-$threads-$workload.txt; do
-			env _NOHEADER=1 csv_from_sysbench.sh $f psfm 60000000 $threads | while read l; do
-												echo "$engine,$configuration,$distribution,$l" >> ../thread_scalability.csv
-												done
-		    done # for f in ../raw
-		done # for configuration
-	    done # for threads
-	done # for engine
-    done # for distribution
-done #for workload
+# raw/memory_scalability/sysbench-mem200-wt-ext4-wt1-pareto-100-oltp.txt
+echo "engine,memory,filesystem,configuration,distribution,workload,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/memory_scalability/sysbench-mem200-wt-ext4-wt1-pareto-100-oltp.txt _ _ _)" > ../memory_scalability.csv
+
+for f in ../raw/memory_scalability/sysbench*txt; do
+    memory=$(echo $f|awk -F'-' '{print $2}'|sed 's/mem//')
+    engine=$(echo $f|awk -F'-' '{print $3}')
+    fs=$(echo $f|awk -F'-' '{print $4}')
+    config=$(echo $f|awk -F'-' '{print $5}')
+    distribution=$(echo $f|awk -F'-' '{print $6}')
+    workload=$(echo $f|awk -F'-' '{print $7}'|sed 's/\.txt//')
+    env _NOHEADER=1 csv_from_sysbench.sh $f psfm 60000000 100 | while read l; do
+								    echo "$engine,$memory,$fs,$config,$distribution,$workload,$l" >> ../memory_scalability.csv
+								done 
+done # for f in ..
