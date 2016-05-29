971646c29b47f31b563652e8c24f3c7945427eec
Fernando Ipar <fipar@acm.org>
2016-05-29 00:38:34 -0300

    added summary

--- scripts/to_csv.sh
+++ scripts/to_csv.sh
@@ -3,7 +3,7 @@
 #It must be executed from within the scripts directory, as it uses relative paths
 # sample file name for this benchmark: 
 # raw/thread_scalability/sysbench-rocks-ext4-rocks0-pareto-1-oltp.txt
-echo "engine,configuration,distribution,workload,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/thread_scalability/sysbench-rocks-ext4-rocks0-pareto-1-oltp.txt _ _ _)" > ../thread_scalability.csv
+echo "engine,configuration,distribution,test,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/thread_scalability/sysbench-rocks-ext4-rocks0-pareto-1-oltp.txt _ _ _)" > ../thread_scalability.csv
 
 for f in ../raw/thread_scalability/sysbench*txt; do
     engine=$(echo $f|awk -F'-' '{print $2}')
@@ -17,7 +17,7 @@ for f in ../raw/thread_scalability/sysbench*txt; do
 done # for f in ..
 
 # raw/memory_scalability/sysbench-mem200-wt-ext4-wt1-pareto-100-oltp.txt
-echo "engine,memory,filesystem,configuration,distribution,workload,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/memory_scalability/sysbench-mem200-wt-ext4-wt1-pareto-100-oltp.txt _ _ _)" > ../memory_scalability.csv
+echo "engine,memory,filesystem,configuration,distribution,test,$(env _ONLYHEADER=1 csv_from_sysbench.sh ../raw/memory_scalability/sysbench-mem200-wt-ext4-wt1-pareto-100-oltp.txt _ _ _)" > ../memory_scalability.csv
 
 for f in ../raw/memory_scalability/sysbench*txt; do
     memory=$(echo $f|awk -F'-' '{print $2}'|sed 's/mem//')
@@ -25,7 +25,7 @@ for f in ../raw/memory_scalability/sysbench*txt; do
     fs=$(echo $f|awk -F'-' '{print $4}')
     config=$(echo $f|awk -F'-' '{print $5}')
     distribution=$(echo $f|awk -F'-' '{print $6}')
-    workload=$(echo $f|awk -F'-' '{print $7}'|sed 's/\.txt//')
+    workload=$(echo $f|awk -F'-' '{print $8}'|sed 's/\.txt//')
     env _NOHEADER=1 csv_from_sysbench.sh $f psfm 60000000 100 | while read l; do
 								    echo "$engine,$memory,$fs,$config,$distribution,$workload,$l" >> ../memory_scalability.csv
 								done 
---
+++ summary/psmdb.md
@@ -0,0 +1,29 @@
+---
+
+title: "Percona Server for MongoDB: CPU and I/O scalability for
+WiredTiger and RocksDB"
+author: "Percona Lab"
+generated on:May 29, 2016
+output:
+  md_document:
+    variant: markdown_github
+
+---
+
+
+# Percona Server for MongoDB 3.2.4-1.rc2 - CPU and I/O scalability 
+
+## Setup
+
+* Setup information pending 
+
+## CPU Scalability tests 
+
+![plot of chunk cpu](figure/cpu-1.png)![plot of chunk cpu](figure/cpu-2.png)![plot of chunk cpu](figure/cpu-3.png)![plot of chunk cpu](figure/cpu-4.png)
+
+## Memory Scalability tests
+
+The memory scalability tests were all done using 100 client threads.
+All graphs faceted by memory and configuration. 
+
+![plot of chunk mem](figure/mem-1.png)![plot of chunk mem](figure/mem-2.png)![plot of chunk mem](figure/mem-3.png)![plot of chunk mem](figure/mem-4.png)![plot of chunk mem](figure/mem-5.png)![plot of chunk mem](figure/mem-6.png)![plot of chunk mem](figure/mem-7.png)![plot of chunk mem](figure/mem-8.png)
